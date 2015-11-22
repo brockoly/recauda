4391c92f84b319da6026db159430cb4d1a2e15b4
brockoly <andres.meneses92@hotmail.cl>
2015-11-22 03:35:15 -0300

    Mantenedor Producto - agregar tipo de producto listo

--- class/Producto.class.php
+++ class/Producto.class.php
@@ -1,18 +1,45 @@
 <?php 
 class Producto{
-	 public $nombre;
-	 public $id;
+	 public $tip_descripcion;
+	 public $tip_prod_id;
 	 
 
 
-	 function setProducto($Producto, $id){
-	 		$this->Producto=$Producto;
-	 		$this->id=$id;
+	 function setProducto($tip_descripcion){
+	 		$this->tip_descripcion=trim($tip_descripcion);
 	 }
+	 function buscarMaximoId($objCon){//
+	 	$sql="SELECT MAX(tip_prod_id)+1 as CONT
+			  FROM tipo_producto";
+		$i=0;
+		$datos;
+		foreach ($objCon->consultaSQL($sql, 'ERROR buscarMaximoId') as $v) {
+			$datos=$v['CONT'];
+	    }
+	    if($datos==""){
+	    	$datos=1;
+	    }
+		return $datos;		 	
+	}
+	 function insertarProducto($objCon){
+	 		$max=$this->buscarMaximoId($objCon);
+		 	$sql ="INSERT INTO tipo_producto(tip_prod_id, tip_descripcion)
+				   VALUES ($max, '$this->tip_descripcion')";
+		 	$rs=$objCon->ejecutarSQL($sql,'ERROR AL insertarProducto');
+		 	return $rs;
+	 }
+	 function buscarProducto($objCon){//
+	 	echo $sql="SELECT tip_descripcion
+			  FROM tipo_producto
+			  WHERE tip_descripcion='$this->tip_descripcion'";
+		$i=0;
+		foreach ($objCon->consultaSQL($sql, 'ERROR buscarProducto') as $v) {
+			$datos=$v['tip_descripcion'];
+			$i++;
+	    }
+		return $datos;		 	
+	}
 
-	 function insertarProducto($conexion, $per_id, $pri_id){
-	 		
-	 }
 
 	 function actualizarProducto($conexion){
 
@@ -22,11 +49,6 @@ class Producto{
 
 
 	 }
-
-	 function buscarProducto($conexion){
-
-	 }
-
 	 function tipoProducto($objCon){
 	 	$sql ="SELECT
 			tipo_producto.tip_prod_id,
