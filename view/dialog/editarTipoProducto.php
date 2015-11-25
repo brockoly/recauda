a7c225f45eedc5247e5fc748fccf2b760ced22b7
brockoly <andres.meneses92@hotmail.cl>
2015-11-25 03:04:54 -0300

    Mantenedor - tipoproducto

--- view/dialog/editarTipoProducto.php
+++ view/dialog/editarTipoProducto.php
@@ -1,3 +1,13 @@
+<?
+	//LLAMADA DE CLASES
+	require_once('../../class/Conectar.class.php'); $objCon = new Conectar(); 
+	require_once('../../class/Producto.class.php'); $objPro = new Producto();
+	//LLAMADA DE METODOS.
+	$objCon->db_connect();
+	$objPro->setProducto('',$tip_prod_id);
+	$unidadMedida = $objPro->listarUMProducto($objCon);
+	$objCon=null;
+?>
 <script type="text/javascript" src="controller/client/js_editarTipoProducto.js"></script>
 <center>
 <form id="frmEditarTipoProducto">
@@ -15,3 +25,30 @@
 		</table><br>
 		</fieldset>
 </form>
+
+<fieldset style=""><legend>Unidades de medida</legend>
+<br>
+	<table class="display" width="100%" id="tblUnidadesMedida">
+            <thead>
+	            <tr>
+	              <th width="60%">Descripción</th>
+	            </tr>
+            </thead>
+            <tbody>
+			<?php
+			for($i=0; $i<count($unidadMedida); $i++){
+		?> 	<tr>
+				<td><?=$unidadMedida[$i]['uni_nombre']?></td>
+				<td>
+					<img title="Editar Tipo Producto" src="./include/img/Edit.png" onclick="ventanaModal('./view/dialog/editarTipoProducto.php','tip_descripcion=<?=$tipoProducto[$i]["tip_descripcion"]?>&tip_prod_id=<?=$tipoProducto[$i]["tip_prod_id"]?>','auto','auto','Editar Tipo Producto','modalEditarTipoProducto')" style="cursor: pointer;')"/>
+					&nbsp;&nbsp;&nbsp;
+					<img title="Eliminar Tipo Producto" src="./include/img/Delete.png" onclick="mensajeUsuarioConProcedimiento('alertMensaje','Confirmar Acción','Atención, se procederá a ELIMINAR el tipo de producto <?=$tipoProducto[$i]["tip_descripcion"]?>, ¿Desea continuar?','./controller/server/controlador_producto.php','tip_prod_id=<?=$tipoProducto[$i]["tip_prod_id"]?>&op=eliminarTipo','./view/dialog/agregarTipoProducto.php','','#modalAgregarProducto')" style="cursor: pointer;" style="cursor: pointer;')"/>
+					&nbsp;&nbsp;&nbsp;
+				</td>
+			</tr>
+			<?php 	}
+			?>	
+            </tbody>
+    </table>
+<br>
+</fieldset>
