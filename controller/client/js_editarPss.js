34e1e90281101256a859f51937f5fb365e7b50cb
soulblue93 <sergio.parra.santiago@gmail.com>
2015-11-30 02:42:25 -0300

    Cambios editar 2:42

--- controller/client/js_editarPss.js
+++ controller/client/js_editarPss.js
@@ -6,7 +6,9 @@
     var productos = [];
     var idSen = 0;
     var idPro = 0;
-    validar('cantPro', 'class' ,'numero');  
+    validar('cantPro', 'class' ,'numero');
+    validar('proCantAgregar', 'class' ,'numero');
+      
 	
     $(".filtroBus").keypress(function(){
        idSen = $(this).attr('id');
@@ -38,29 +40,35 @@
             id = $(this).attr('id');
             codigo = '<td align="center" class="cuerpoDatosTablas">'+ui.item.id+'</td>';
             descripcion = '<td class="cuerpoDatosTablas" align="center">'+ui.item.value+'</td>';
-            $("#cantPro"+id).show("slow");     
+            $("#cantPro"+id).show("slow");
+            $("#cantPro"+id).focus();     
         }
     });
 
     $(".cantPro").blur(function(){
         $("#tblProducto"+id).show("slow");
+        var x =parseInt($("#cantPro"+id).val());
+        if(x==0){
+            x=1;
+        }
+        var y = parseInt($("#cantProducto"+id).val());
         if(productos.length==0){                    
             cantidad="";
-            cantidad = '<td class="cuerpoDatosTablas" align="center" width="10%"><input type="text" style="width:60px" id="cantProducto'+idPro+'" value="'+$("#cantPro"+id).val()+'" /></td>';
-            $("#tblProducto"+id).append("<tr>"+codigo+descripcion+cantidad+"</tr>");
+            cantidad = '<td class="cuerpoDatosTablas" align="center" width="10%"><input  class="proCantAgregar" type="text" style="width:60px" id="cantProducto'+idPro+'" value="'+x+'" /></td>';
+            $("#tblProducto"+id).append("<tr id='eliminarPro"+id+"'>"+codigo+descripcion+cantidad+"</tr>");
             $("#cantPro"+id).val("");
             $("#cantPro"+id).hide();            
             productos[productos.length]=idPro;
         }else{
             if(jQuery.inArray( id, productos )==-1){
                 cantidad="";
-                cantidad = '<td class="cuerpoDatosTablas" align="center" width="10%"><input type="text" style="width:60px" id="cantProducto'+idPro+'" value="'+$("#cantPro"+id).val()+'" /></td>';
-                $("#tblProducto"+id).append("<tr>"+codigo+descripcion+cantidad+"</tr>");
+                cantidad = '<td class="cuerpoDatosTablas" align="center" width="10%"><input class="proCantAgregar"  type="text" style="width:60px" id="cantProducto'+idPro+'" value="'+$("#cantPro"+id).val()+'" /></td>';
+                $("#tblProducto"+id).append("<tr id='eliminarPro"+id+"'>"+codigo+descripcion+cantidad+"</tr>");
                 $("#cantPro"+id).val("");
                 $("#cantPro"+id).hide();
                 productos[productos.length]=idPro;
             }else{               
-               $("#cantProducto"+id).val(parseInt(x)+parseInt(y));
+               $("#cantProducto"+id).val(x+y);
                $("#cantPro"+id).val("");
                $("#cantPro"+id).hide();
             }
